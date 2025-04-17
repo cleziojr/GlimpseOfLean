ac2c9c96ca2938bcf967e9d678f2f01f63ec14fb
Clézio <cleziojr9@gmail.com>
2025-04-16 21:00:31 -0300

    :feat: 02Iff.lean concluído

--- GlimpseOfLean/Exercises/02Iff.lean
+++ GlimpseOfLean/Exercises/02Iff.lean
@@ -89,7 +89,11 @@ example (a : ℝ) : a > 0 → b > 0 → a + b > 0 := by {
 /- Now prove the following simple statement in propositional logic.
 Note that `p → q → r` means `p → (q → r)`. -/
 example (p q r : Prop) : (p → q) → (p → q → r) → p → r := by {
-  sorry
+  intro ha hb hc
+  apply hb
+  apply hc
+  apply ha
+  exact hc
 }
 
 /- # Equivalences
@@ -120,7 +124,10 @@ Let's prove a variation
 -/
 
 example {a b : ℝ} (c : ℝ) : a + c ≤ b + c ↔ a ≤ b := by {
-  sorry
+  rw [← sub_nonneg]
+  have key : (b+c) - (a+c) = b - a := by ring
+  rw [key]
+  rw [sub_nonneg]
 }
 
 /-
@@ -158,7 +165,9 @@ example {a b : ℝ}  (ha : 0 ≤ a) : b ≤ a + b := by {
 /- Let's do a variant using `add_le_add_iff_left a : a + b ≤ a + c ↔ b ≤ c` instead. -/
 
 example (a b : ℝ) (hb : 0 ≤ b) : a ≤ a + b := by {
-  sorry
+  calc
+    a = a + 0 := by ring
+    _ ≤ a + b := by exact (add_le_add_iff_left a).2 hb
 }
 
 /-
@@ -189,7 +198,18 @@ example (a b : ℝ) : (a-b)*(a+b) = 0 ↔ a^2 = b^2 := by {
 /- You can try it yourself in this exercise. -/
 
 example (a b : ℝ) : a = b ↔ b - a = 0 := by {
-  sorry
+  constructor
+  · intro h
+    calc
+      b - a = 0 + (b-a) := by ring
+      _ = 0 + (b-b) := by rw[h]
+      _ = 0 := by ring
+  · intro h
+    calc
+      a = b + (a-b) := by ring
+      _ = b - (b-a) := by ring
+      _ = b - 0     := by rw [h]
+      _ = b         := by ring
 }
 
 /-
